e37092c9fb1d4a3ca2e8a97495456b52b3f6eac8
piaspro <golamrahman09@gmail.com>
2020-07-23 08:00:51 +0600

    update

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-// Adding item to the cart
+// // Common function for purse float
+// function floatingNumber(id) {
+//     const input = document.getElementById(id).value;
+//     const number = parseFloat(input);
+//     return number;
+// }
+
+
+// Adding iphone to the cart
 const addNumber = document.getElementById('add-item');
 addNumber.addEventListener('click', function addingItem() {
     const input = document.getElementById('numberInput').value;
@@ -22,7 +30,7 @@ addNumber.addEventListener('click', function addingItem() {
     document.getElementById('totalPrice').innerText = totalAmount + 1231;
 })
 
-// Removing item from the cart
+// Removing iphone from the cart
 const removeNumber = document.getElementById('remove-item');
 removeNumber.addEventListener('click', function removingItem() {
     const input = document.getElementById('numberInput').value;
@@ -44,4 +52,45 @@ removeNumber.addEventListener('click', function removingItem() {
     const amount = document.getElementById('totalPrice').innerText;
     const totalAmount = parseFloat(amount);
     document.getElementById('totalPrice').innerText = totalAmount - 1231;
+})
+
+// Adding iphone cover to the cart
+const addCover = document.getElementById('add-cover');
+addCover.addEventListener('click', function addingItem() {
+    const input = document.getElementById('coverInput').value;
+    const number = parseFloat(input);
+    document.getElementById('coverInput').value = number + 1;
+
+    const current = document.getElementById('coverPrice').innerText;
+    const currentAmount = parseFloat(current);
+    document.getElementById('coverPrice').innerText = currentAmount + 59;
+
+    const present = document.getElementById('subTotalPrice').innerText;
+    const presentAmount = parseFloat(present);
+    document.getElementById('subTotalPrice').innerText = presentAmount + 59;
+
+    const amount = document.getElementById('totalPrice').innerText;
+    const totalAmount = parseFloat(amount);
+    document.getElementById('totalPrice').innerText = totalAmount + 59;
+})
+
+
+// Remove iphone cover to the cart
+const removeCover = document.getElementById('remove-cover');
+removeCover.addEventListener('click', function addingItem() {
+    const input = document.getElementById('coverInput').value;
+    const number = parseFloat(input);
+    document.getElementById('coverInput').value = number - 1;
+
+    const current = document.getElementById('coverPrice').innerText;
+    const currentAmount = parseFloat(current);
+    document.getElementById('coverPrice').innerText = currentAmount - 59;
+
+    const present = document.getElementById('subTotalPrice').innerText;
+    const presentAmount = parseFloat(present);
+    document.getElementById('subTotalPrice').innerText = presentAmount - 59;
+
+    const amount = document.getElementById('totalPrice').innerText;
+    const totalAmount = parseFloat(amount);
+    document.getElementById('totalPrice').innerText = totalAmount - 59;
 })
