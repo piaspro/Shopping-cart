1f0d2e6f2e0e99c1a09872012c443875e811b2d7
piaspro <golamrahman09@gmail.com>
2020-07-23 19:40:03 +0600

    finished

--- script.js
+++ script.js
@@ -1,96 +1,66 @@
 // // Common function for purse float
-// function floatingNumber(id) {
-//     const input = document.getElementById(id).value;
-//     const number = parseFloat(input);
-//     return number;
-// }
+function floatingNumber(id) {
+    const input = document.getElementById(id).value;
+    const number = parseFloat(input);
+    return number;
+}
+
+function floatingTextNumber(id) {
+    const inputText = document.getElementById(id).innerText;
+    const inputNumber = parseFloat(inputText);
+    return inputNumber;
+}
 
 
 // Adding iphone to the cart
 const addNumber = document.getElementById('add-item');
 addNumber.addEventListener('click', function addingItem() {
-    const input = document.getElementById('numberInput').value;
-    const number = parseFloat(input);
-    document.getElementById('numberInput').value = number + 1;
+    document.getElementById('numberInput').value = floatingNumber('numberInput') + 1;
 
-    const current = document.getElementById('iphonePrice').innerText;
-    const currentAmount = parseFloat(current);
-    document.getElementById('iphonePrice').innerText = currentAmount + 1219;
+    document.getElementById('iphonePrice').innerText = floatingTextNumber('iphonePrice') + 1219;
 
-    const present = document.getElementById('subTotalPrice').innerText;
-    const presentAmount = parseFloat(present);
-    document.getElementById('subTotalPrice').innerText = presentAmount + 1219;
+    document.getElementById('subTotalPrice').innerText = floatingTextNumber('subTotalPrice') + 1219;
 
-    const presentTax = document.getElementById('tax').innerText;
-    const totalTax = parseFloat(presentTax);
-    document.getElementById('tax').innerText = totalTax + 12;
+    document.getElementById('tax').innerText = floatingTextNumber('tax') + 12;
 
-    const amount = document.getElementById('totalPrice').innerText;
-    const totalAmount = parseFloat(amount);
-    document.getElementById('totalPrice').innerText = totalAmount + 1231;
+    document.getElementById('totalPrice').innerText = floatingTextNumber('totalPrice') + 1231;
 })
 
 // Removing iphone from the cart
 const removeNumber = document.getElementById('remove-item');
 removeNumber.addEventListener('click', function removingItem() {
-    const input = document.getElementById('numberInput').value;
-    const number = parseFloat(input);
-    document.getElementById('numberInput').value = number - 1;
+    document.getElementById('numberInput').value = floatingNumber('numberInput') - 1;
 
-    const current = document.getElementById('iphonePrice').innerText;
-    const currentAmount = parseFloat(current);
-    document.getElementById('iphonePrice').innerText = currentAmount - 1219;
+    document.getElementById('iphonePrice').innerText = floatingTextNumber('iphonePrice') - 1219;
 
-    const present = document.getElementById('subTotalPrice').innerText;
-    const presentAmount = parseFloat(present);
-    document.getElementById('subTotalPrice').innerText = presentAmount - 1219;
+    document.getElementById('subTotalPrice').innerText = floatingTextNumber('subTotalPrice') - 1219;
 
-    const presentTax = document.getElementById('tax').innerText;
-    const totalTax = parseFloat(presentTax);
-    document.getElementById('tax').innerText = totalTax - 12;
+    document.getElementById('tax').innerText = floatingTextNumber('tax') - 12;
 
-    const amount = document.getElementById('totalPrice').innerText;
-    const totalAmount = parseFloat(amount);
-    document.getElementById('totalPrice').innerText = totalAmount - 1231;
+    document.getElementById('totalPrice').innerText = floatingTextNumber('totalPrice') - 1231;
 })
 
 // Adding iphone cover to the cart
 const addCover = document.getElementById('add-cover');
 addCover.addEventListener('click', function addingItem() {
-    const input = document.getElementById('coverInput').value;
-    const number = parseFloat(input);
-    document.getElementById('coverInput').value = number + 1;
+    document.getElementById('coverInput').value = floatingNumber('coverInput') + 1;
 
-    const current = document.getElementById('coverPrice').innerText;
-    const currentAmount = parseFloat(current);
-    document.getElementById('coverPrice').innerText = currentAmount + 59;
+    document.getElementById('coverPrice').innerText = floatingTextNumber('coverPrice') + 59;
 
-    const present = document.getElementById('subTotalPrice').innerText;
-    const presentAmount = parseFloat(present);
-    document.getElementById('subTotalPrice').innerText = presentAmount + 59;
+    document.getElementById('subTotalPrice').innerText = floatingTextNumber('subTotalPrice') + 59;
 
-    const amount = document.getElementById('totalPrice').innerText;
-    const totalAmount = parseFloat(amount);
-    document.getElementById('totalPrice').innerText = totalAmount + 59;
+    document.getElementById('totalPrice').innerText = floatingTextNumber('totalPrice') + 59;
 })
 
 
 // Remove iphone cover to the cart
 const removeCover = document.getElementById('remove-cover');
 removeCover.addEventListener('click', function addingItem() {
-    const input = document.getElementById('coverInput').value;
-    const number = parseFloat(input);
-    document.getElementById('coverInput').value = number - 1;
+    document.getElementById('coverInput').value = floatingNumber('coverInput') - 1;
 
-    const current = document.getElementById('coverPrice').innerText;
-    const currentAmount = parseFloat(current);
-    document.getElementById('coverPrice').innerText = currentAmount - 59;
+    document.getElementById('coverPrice').innerText = floatingTextNumber('coverPrice') - 59;
 
-    const present = document.getElementById('subTotalPrice').innerText;
-    const presentAmount = parseFloat(present);
-    document.getElementById('subTotalPrice').innerText = presentAmount - 59;
+    document.getElementById('subTotalPrice').innerText = floatingTextNumber('subTotalPrice') - 59;
 
-    const amount = document.getElementById('totalPrice').innerText;
-    const totalAmount = parseFloat(amount);
-    document.getElementById('totalPrice').innerText = totalAmount - 59;
+    document.getElementById('totalPrice').innerText = floatingTextNumber('totalPrice') - 59;
 })
